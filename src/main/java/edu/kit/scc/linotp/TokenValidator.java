681411daf8d5c29921ae93a87821974d53570ac1
Michael Simon <simon@kit.edu>
2017-02-21 10:30:31 +0100

    Found the correct place to define classified message
    Can now use the classified message map. Should be better with i18n and
    more possible error messages.

--- src/main/java/edu/kit/scc/linotp/TokenValidator.java
+++ src/main/java/edu/kit/scc/linotp/TokenValidator.java
@@ -3,6 +3,10 @@ package edu.kit.scc.linotp;
 import javax.annotation.Nonnull;
 import javax.security.auth.Subject;
 
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.shibboleth.idp.authn.AbstractValidationAction;
 import net.shibboleth.idp.authn.AuthnEventIds;
 import net.shibboleth.idp.authn.context.AuthenticationContext;
@@ -12,11 +16,6 @@ import net.shibboleth.idp.session.context.SessionContext;
 import net.shibboleth.utilities.java.support.annotation.constraint.NotEmpty;
 import net.shibboleth.utilities.java.support.component.ComponentSupport;
 import net.shibboleth.utilities.java.support.primitive.StringSupport;
-
-import org.opensaml.profile.context.ProfileRequestContext;
-import org.opensaml.saml.common.SAMLObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class TokenValidator extends AbstractValidationAction {
 
@@ -91,13 +90,13 @@ public class TokenValidator extends AbstractValidationAction {
 				return;
 			}
 				
-			handleError(profileRequestContext, authenticationContext, new LinotpLoginException("InvalidCredentials"),
+			handleError(profileRequestContext, authenticationContext, "TokenWrong",
 						AuthnEventIds.INVALID_CREDENTIALS);
 	
 		}		
 		catch (Exception e) {
 			logger.warn("{} Exception while validating token: {}", getLogPrefix(), e.getMessage());
-			handleError(profileRequestContext, authenticationContext, new LinotpLoginException("GenericException"),
+			handleError(profileRequestContext, authenticationContext, e,
 					AuthnEventIds.AUTHN_EXCEPTION);
 		}
 	}
