48e6eacd34d8490baed040bd04f7c008fa6d23ba
Ian Bobbitt <ian@icb.im>
2020-03-26 16:39:15 -0400

    Support for IdP4

--- src/main/java/edu/kit/scc/linotp/ExtractTokenFromForm.java
+++ src/main/java/edu/kit/scc/linotp/ExtractTokenFromForm.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2017 Michael Simon
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -33,8 +33,8 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Strings;
 
-public class ExtractTokenFromForm extends AbstractExtractionAction<SAMLObject, SAMLObject> {
-	
+public class ExtractTokenFromForm extends AbstractExtractionAction {
+
 	/** Class logger. */
 	@Nonnull
 	private final Logger logger = LoggerFactory.getLogger(ExtractTokenFromForm.class);
@@ -57,9 +57,9 @@ public class ExtractTokenFromForm extends AbstractExtractionAction<SAMLObject, S
 
 
 	@Override
-	protected void doExecute(@Nonnull final ProfileRequestContext<SAMLObject, SAMLObject> profileRequestContext,
+	protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext,
 			@Nonnull final AuthenticationContext authenticationContext) {
-		
+
 		final HttpServletRequest request = getHttpServletRequest();
 
 		if (request == null) {
@@ -92,6 +92,6 @@ public class ExtractTokenFromForm extends AbstractExtractionAction<SAMLObject, S
 
 		} catch (Exception e) {
 			logger.warn("{} Login by {} produced exception", getLogPrefix(),  e);
-		}		
+		}
 	}
 }
